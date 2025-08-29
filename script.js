f2981e56832e8a14fb9a24bf5525f6f17ba88a57
DevUmair00 <rkumair203@gmail.com>
2025-08-29 23:24:11 +0500

    Fix Multiple Operator Bug

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ let string = "";
 let arr = Array.from(buttons);
 
 arr.forEach(buttons => {
-    buttons.addEventListener(`click` , (e) => {
-      if (e.target.innerHTML == "=") {
-        string = eval(string)
+  buttons.addEventListener(`click`, (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string)
+      input.value = string;
+      string = "";
+    }
+    else if (e.target.innerHTML == "AC") {
+      string = ""
+      input.value = string;
+    }
+    else if (e.target.innerHTML == "DEL") {
+      string = input.value.slice(0, string.length - 1)
+      input.value = string;
+    }
+    else if (e.target.innerHTML == "%" || e.target.innerHTML == "/" || e.target.innerHTML == "+" || e.target.innerHTML == "-" || e.target.innerHTML == "*") {
+      let lastChar = string.slice(-1);
+      if (lastChar == "%" || lastChar == `/` || lastChar == `+` || lastChar == `-` || lastChar == `*`) {
+
         input.value = string;
       }
-      else if (e.target.innerHTML == "AC") {
-        string = ""
-        input.value = string;
-      }    
-      else if (e.target.innerHTML == "DEL") {
-        string = input.value.slice(0 , string.length - 1)
-        input.value = string;
-      }    
-      else
-      {
+      else {
         string += e.target.innerHTML;
         input.value = string;
       }
-    })
+    }
+    else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  })
 })
